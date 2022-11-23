71265126191a824e8b0a287773f9fb1e4b64285c
罗进杰 <451175068@qq.com>
2022-11-23 15:26:24 +0800

    20221122 878. 第 N 个神奇数字

---
+++ javascript/202211/22nd.js
@@ -0,0 +1,42 @@
+"use strict";
+/**
+ * 878. 第 N 个神奇数字
+ * 一个正整数如果能被 a 或 b 整除，那么它是神奇的。
+ * 给定三个整数 n , a , b ，返回第 n 个神奇的数字。因为答案可能很大，所以返回答案 对 109 + 7 取模 后的值。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode.cn/problems/nth-magical-number/solutions/
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+function nthMagicalNumber(n, a, b) {
+    let mod = 1e9 + 7;
+    let left = Math.min(a, b); // 左边界
+    let right = left * n; // 右边界
+    let c = lcm(a, b); // 最小公倍数
+    while (left <= right) {
+        let middle = left + Math.floor((right - left) / 2);
+        // 小于等于middle的神奇数字个数
+        // Math.floor(middle / a) 被a整除的个数
+        // Math.floor(middle / b) 被b整除的个数
+        // Math.floor(middle / c) 同时被a、b整除的个数
+        let count = Math.floor(middle / a) + Math.floor(middle / b) - Math.floor(middle / c);
+        if (count >= n) {
+            right = middle - 1;
+        }
+        else {
+            left = middle + 1;
+        }
+    }
+    return (right + 1) % mod;
+}
+;
+// 最大公约数
+function gcd(a, b) {
+    return b == 0 ? a : gcd(b, a % b);
+}
+// 最小公倍数
+function lcm(a, b) {
+    return a * b / gcd(a, b);
+}
+console.log(nthMagicalNumber(4, 2, 3));
+//# sourceMappingURL=22nd.js.map
