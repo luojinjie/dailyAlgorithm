5f8746ab89b328e4dbd9f57b20e547b16ac702f5
罗进杰 <451175068@qq.com>
2023-04-12 13:52:45 +0800

    20230412 1147. 段式回文

---
+++ javascript/202304/12th.js
@@ -0,0 +1,33 @@
+"use strict";
+/**
+ * 1147. 段式回文
+ * 你会得到一个字符串 text 。你应该把它分成 k 个子字符串 (subtext1, subtext2，…， subtextk) ，要求满足:
+ * subtexti 是 非空 字符串
+ * 所有子字符串的连接等于 text ( 即subtext1 + subtext2 + ... + subtextk == text )
+ * 对于所有 i 的有效值( 即 1 <= i <= k ) ，subtexti == subtextk - i + 1 均成立
+ * 返回k可能最大值。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode.cn/problems/longest-chunked-palindrome-decomposition/
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+function longestDecomposition(text) {
+    let n = text.length;
+    if (n == 0) {
+        return 0;
+    }
+    // 贪心
+    let i = 1;
+    let j = text.length - 1;
+    while (i <= j) {
+        if (text.substring(0, i) == text.substring(j, n)) {
+            return 2 + longestDecomposition(text.substring(i, j));
+        }
+        i++;
+        j--;
+    }
+    return 1;
+}
+;
+console.log(longestDecomposition("elvtoelvto"));
+//# sourceMappingURL=12th.js.map
